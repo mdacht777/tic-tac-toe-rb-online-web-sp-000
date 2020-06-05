c47cc32e6bc972da4c10c1ce6c480c3c8c724621
Martin Achtermann <mdacht777@gmail.com>
2020-06-05 01:36:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def turn(board)
+def turn(board,current_player(board))
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
